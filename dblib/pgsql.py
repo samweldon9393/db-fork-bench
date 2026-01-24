ca0e18500ab12fd4b5364bad0a95e584e31e193b
Sam <samweldon9393@gmail.com>
2026-01-24 14:03:25 -0500

    pgsql.py working, branch creation working, benchmarks run and look legit, just need to add some cleanup because right now all branches remain on disk as dbs after running scripts

--- dblib/pgsql.py
+++ dblib/pgsql.py
@@ -54,21 +54,60 @@ class PgsqlToolSuite(DBToolSuite):
         self._connection_uri = connection_uri
         self.autocommit = autocommit
 
-        self.current_branch = "main"
-
+        cmd = "SELECT CURRENT_DATABASE();"
+        res = super().execute_sql(cmd)
+        self.current_branch_name = res[0][0]
+        self._all_branches = {self.current_branch_name: connection_uri}
 
     def get_uri_for_db_setup(self) -> str:
         """Returns the connection URI for database setup operations (e.g., PGSQL)."""
         return self._connection_uri
 
-    def _create_branch_impl(self, branch_name: str, parent_id: str) -> None:
-        cmd = f"CREATE DATABASE {branch_name} TEMPLATE {parent_id} STATEGY = FILE_COPY"
+    def delete_db(self, db_name: str) -> None:
+        """
+        Deletes the database from all branches in the Neon project.
+        """
+        cmd = f"DROP DATABASE {db_name}"
+        try:
+            super().execute_sql(cmd)
+        except Exception as e:
+            raise Exception(f"Error deleting database: {e}")
+        
+
+    # Use parent_name instead of parent_id since there's no inherent id 
+    # so it is simpler to just use names
+    def _create_branch_impl(self, branch_name: str, parent_name: str) -> None:
+        cmd = f"CREATE DATABASE {branch_name} TEMPLATE {parent_name} STRATEGY = FILE_COPY"
         super().execute_sql(cmd)
-        self.current_branch = branch_name
+        self.current_branch_name = branch_name
+        uri = dbutil.format_db_uri(
+            PGSQL_USER, PGSQL_PASSWORD, PGSQL_HOST, PGSQL_PORT, branch_name
+        )
+        self._all_branches[branch_name] = uri
 
     def _connect_branch_impl(self, branch_name: str) -> None:
-        cmd = f"\\c {branch_name}"
-        super().execute_sql(cmd)
+        uri = self._all_branches[branch_name]
+        if not branch_name:
+            all_branches = self._get_pgsql_branches()
+            if branch_name not in all_branches:
+                raise ValueError(f"Branch '{branch_name}' does not exist.")
+            branch_id = all_branches[branch_name]
+        if not uri:
+            uri = dbutil.format_db_uri(
+                PGSQL_USER, PGSQL_PASSWORD, PGSQL_HOST, PGSQL_PORT, branch_name
+            )
+            # Cache the URI - replace tuple since tuples are immutable
+            self._all_branches[branch_name] = uri
+
+        self.conn.close()
+        self.conn = psycopg2.connect(uri)
+        if self.autocommit:
+            self.conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        self.current_branch_name = branch_name
 
     def _get_current_branch_impl(self) -> tuple[str, str]:
-        return (self.current_branch, 0) # branch_id not implemented
+        return (self.current_branch_name, self.current_branch_name) # branch_id not implemented
+
+    def _get_pgsql_branches(self):
+        # TODO implemente
+        pass
